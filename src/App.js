67ba81fa5654ad578ef36cf723dcd1a520785654
Rahyan <rahyanakil89@gmail.com>
2022-09-22 02:06:26 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Person></Person>
+      <Person></Person>
+      <Person></Person>
+      <h3>Here we have next similar in look different in data.</h3>
+      <Employee></Employee>
+      <Employee></Employee>
+      <Employee></Employee>
     </div>
   );
 }
+
+
+function Person(){
+  return(
+    <div className='profile'>
+      <h1>Rahyan Shamsi</h1>
+      <p>Junior Front-End Dev</p>
+    </div>
+  );
+}
+
+function Employee(){
+  return(
+    <div className='container'>
+      <h1>Sakil Ahmed</h1>
+      <p>Full stack Dev</p>
+    </div>
+  );
+}
+
 
 export default App;
